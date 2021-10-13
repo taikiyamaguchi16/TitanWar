65866489b11e6257e971dad0cdf7be2f57a262db
KentaOishi <justicekenta@i.softbank.jp>
2021-10-13 17:27:22 +0900

    [update]スポナースクリプトを調整

--- Assets/Jasu/Script/general/Spawner.cs
+++ Assets/Jasu/Script/general/Spawner.cs
@@ -11,62 +11,54 @@ public enum HowToSpawn
     Interval,
 }
 
-public class Spawner : MonoBehaviour
+[System.Serializable]
+public class SpawnInfo
 {
-    [SerializeField, Tooltip("スポーンされるプレハブ")]
-    GameObject prefabToSpawn;
+    [Tooltip("スポーンされるプレハブ")]
+    public GameObject prefabToSpawn;
 
-    [SerializeField, Tooltip("生成位置")]
-    Vector3 spawnPosition;
+    [Tooltip("生成Transform(manualオフ)")]
+    public Transform spawnTrans = null;
 
-    [SerializeField, Tooltip("生成角度")]
-    Vector3 spawnEuler;
+    [Tooltip("生成位置(manualオン)")]
+    public Vector3 spawnPosition;
 
-    [SerializeField,Tooltip("生成方法")]
-    HowToSpawn howToSpawn;
+    [Tooltip("生成角度(manualオン)")]
+    public Vector3 spawnEuler;
 
-    [SerializeField, Tooltip("インターバル秒数")]
-    float intervalSeconds = 1f;
+    [Tooltip("自動生成の方法")]
+    public HowToSpawn howToSpawn;
 
-    float timer = 0f;
+    [Tooltip("インターバル秒数")]
+    public float intervalSeconds = 1f;
 
-    [SerializeField, Tooltip("スタート時に生成するか否か")]
-    bool spawnWhenStart = true;
+    [HideInInspector]
+    public float timer = 0f;
+
+    [Tooltip("trueなら入力した位置と向き、falseならアタッチされたTransformにスポーン、")]
+    public bool manual = true;
+
+    [Tooltip("スタート時に生成するか否か")]
+    public bool spawnWhenStart = true;
 
     [Tooltip("スポーン可能かどうか")]
     public bool spawnable = true;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (spawnWhenStart && spawnable)
-            SpawnPrefab();
-    }
-
-    private void Update()
-    {
-        switch (howToSpawn)
-        {
-            case HowToSpawn.Interval:
-                timer += Time.deltaTime;
-                if(timer >= intervalSeconds)
-                {
-                    SpawnPrefab();
-                    timer = 0;
-                }
-                break;
-            default:
-                break;
-        }
-    }
 
     public GameObject SpawnPrefab()
     {
         if (spawnable)
         {
-            GameObject spawned = Instantiate(prefabToSpawn);
-            spawned.transform.position = spawnPosition;
-            spawned.transform.rotation = Quaternion.Euler(spawnEuler);
+            GameObject spawned = Object.Instantiate(prefabToSpawn);
+            if (manual)
+            {
+                spawned.transform.position = spawnPosition;
+                spawned.transform.rotation = Quaternion.Euler(spawnEuler);
+            }
+            else
+            {
+                spawned.transform.position = spawnTrans.position;
+                spawned.transform.rotation = spawnTrans.rotation;
+            }
             return spawned;
         }
         return null;
@@ -76,11 +68,53 @@ public class Spawner : MonoBehaviour
     {
         if (spawnable)
         {
-            GameObject spawned = Instantiate(prefabToSpawn);
+            GameObject spawned = Object.Instantiate(prefabToSpawn);
             spawned.transform.position = _pos;
             spawned.transform.rotation = Quaternion.Euler(_euler);
             return spawned;
         }
         return null;
+    }
+
+    public GameObject SpawnPrefab(Transform _spawnTrans)
+    {
+        if (spawnable)
+        {
+            GameObject spawned = Object.Instantiate(prefabToSpawn);
+            spawned.transform.position = _spawnTrans.position;
+            spawned.transform.rotation = _spawnTrans.rotation;
+            return spawned;
+        }
+        return null;
+    }
+}
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField,Tooltip("スポーン情報")]
+    SpawnInfo spawnInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (spawnInfo.spawnWhenStart && spawnInfo.spawnable)
+            spawnInfo.SpawnPrefab();
+    }
+
+    private void Update()
+    {
+        switch (spawnInfo.howToSpawn)
+        {
+            case HowToSpawn.Interval:
+                spawnInfo.timer += Time.deltaTime;
+                if(spawnInfo.timer >= spawnInfo.intervalSeconds)
+                {
+                    spawnInfo.SpawnPrefab();
+                    spawnInfo.timer = 0;
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
