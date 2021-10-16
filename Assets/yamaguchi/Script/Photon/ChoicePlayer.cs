51f97cfe7bb45f71fb7b7e29f48fbb8b563378f8
tai <taikitpmiita@icloud.com>
2021-10-16 22:46:32 +0900

    [update]仮でサーバーを設定

---
+++ Assets/yamaguchi/Script/Photon/ChoicePlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+public class ChoicePlayer : MonoBehaviourPunCallbacks
+{
+    //各デバイス選択時の生成するオブジェクト
+    public GameObject vr_player;
+    public GameObject pc_player;
+    //遷移するシーンの名前
+    public string sceneName;
+    //最終的に生成するオブジェクト
+    private GameObject spownPlayer;
+    //ロビー接続時にアクティブにするため
+    public GameObject choicebutton;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.activeSceneChanged += ActiveSceneChanged;
+    }
+
+    public void ChoiceVR()
+    {
+        spownPlayer = vr_player;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ChoicePC()
+    {
+        spownPlayer = pc_player;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        choicebutton.SetActive(true);
+    }
+
+    void ActiveSceneChanged(Scene thisScene, Scene nextScene)
+    {
+        // 上で取得した場所に、"bullet"のPrefabを出現させる
+        GameObject newBall = Instantiate(spownPlayer,Vector3.zero, transform.rotation);
+        Debug.Log(nextScene.name);
+    }
+}
---
+++ Assets/yamaguchi/Script/Photon/PhotonManager.cs
@@ -0,0 +1,66 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using ExitGames.Client.Photon;
+using UnityEngine.SceneManagement;
+
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    public bool isOffline = false;
+
+    public void Connect()
+    {
+        if (!isOffline)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            PhotonNetwork.OfflineMode = true;
+        }
+    }
+
+    public void Disconnect()
+    {
+        if (!isOffline)
+        {
+            PhotonNetwork.Disconnect();
+        }
+        else
+        {
+            PhotonNetwork.OfflineMode = false;
+        }
+    }
+
+    private void Start()
+    {
+        PhotonNetwork.NickName = "Player";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        // ランダムなルームに参加する
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    // ランダムで参加できるルームが存在しないなら、新規でルームを作成する
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        // ルームの参加人数を2人に設定する
+        var roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 4;
+
+        PhotonNetwork.CreateRoom(null, roomOptions);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            //PhotonNetwork.CurrentRoom.SetStartTime(PhotonNetwork.ServerTimestamp);
+        }
+        //SceneManager.LoadScene("Test_Item");
+    }
+}
