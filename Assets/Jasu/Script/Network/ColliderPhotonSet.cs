dec39e84e8d571b215ca52ca65cb9a39d23836f0
KentaOishi <justicekenta@i.softbank.jp>
2021-10-19 00:32:17 +0900

    [update]Titan更新、公式からコライダー制御を奪取

---
+++ Assets/Jasu/Script/Network/ColliderPhotonSet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ColliderPhotonSet : MonoBehaviour
+{
+    [SerializeField]
+    List<Collider> colliderList = new List<Collider>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider[] colliders =
+            this.GetComponentsInChildren<Collider>().Where(childCollider => !childCollider.isTrigger).ToArray();
+        colliderList.AddRange(colliders);
+
+        foreach (var col in colliderList)
+        {
+            col.gameObject.AddComponent<PhotonTransformView>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Jasu/Script/VR/HandCollider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class HandCollider : MonoBehaviour
+{
+    [SerializeField]
+    Collider[] m_colliders = null;
+
+    bool firstUpdate = false;
+
+    void Awake()
+    {
+        m_colliders = this.GetComponentsInChildren<Collider>().Where(childCollider => !childCollider.isTrigger).ToArray();
+        foreach (var c in m_colliders)
+        {
+            c.isTrigger = true;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstUpdate = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (firstUpdate)
+        {
+            foreach (var c in m_colliders)
+            {
+                c.isTrigger = false;
+                c.enabled = true;
+            }
+            firstUpdate = false;
+        }
+    }
+}
