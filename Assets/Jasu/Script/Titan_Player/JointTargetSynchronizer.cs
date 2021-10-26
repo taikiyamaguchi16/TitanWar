ea09db1ec66af5ffe785f228b6edb3afd79f5aaf
KentaOishi <justicekenta@i.softbank.jp>
2021-10-26 12:22:52 +0900

    [update]VRの手の非貫通処理追加

---
+++ Assets/Jasu/Script/Titan_Player/JointTargetSynchronizer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointTargetSynchronizer : MonoBehaviour
+{
+    [SerializeField]
+    ConfigurableJoint configurableJoint;
+
+    [SerializeField]
+    Transform controllerTrans;
+
+    [SerializeField]
+    Transform synchronizedTargetTrans = null;
+
+    [SerializeField]
+    float localPositionMultiply = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        configurableJoint.targetPosition = controllerTrans.localPosition * localPositionMultiply;
+        synchronizedTargetTrans.localRotation = controllerTrans.localRotation;
+    }
+}
