9bfed2f63cf1224e09fa4745e911a459952eb038
Yuichi Nishikori <nishikori@pmc.co.jp>
2021-11-16 12:26:49 +0900

    [update] アイテム修正

--- Assets/nishiko/script/ItemBoxManager.cs
+++ Assets/nishiko/script/ItemBoxManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class ItemBoxManager : MonoBehaviour
 {
+    [Header("生成間隔(秒数)")]
+    [SerializeField] private int m_interval = 30;
+
+    //開始何秒まではレアアイテムを出さないようにする
+    private int NonrareSeconds = 30;
+    private bool timeOverNonrare = true;
+
+
+
     [Header("アイテムボックス")]
     [SerializeField]
     private ItemBox[] m_objItembox;
@@ -18,63 +27,72 @@ public class ItemBoxManager : MonoBehaviour
     //確率確認用
     float compRate;
 
-    //[Header("それぞれの確率(上からレア度が低い)")]
-    //public float[] popRate = new float[3];
-    //private float popNum;
+    [Header("それぞれの確率(上からレア度が低い)")]
+    public float[] popRate = new float[3];
+    private float[] saveRate = new float[3];    //保存用Rate
+    private float popNum;
+
+
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
         m_MaxNum = m_objItembox.Length; //アイテムボックスの最大数を確定
-        //AverageRate(); //確率を綺麗にする。
+        AverageRate(); //確率を綺麗にする。
+
+        for (int i = 0; i < popRate.Length; i++)
+        {
+            saveRate[i] = popRate[i];   //保存する。
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_myTime += Time.deltaTime;
+        m_myTime += Time.deltaTime;//時間を経過させる
+        int second = (int)m_myTime % 60;
 
-        ////レア度を確定させる
-        //switch (RareDecision())
-        //{
-        //    //ここでレア度に分けて生成させる
-        //    case Item.ITEMRATE.NORMAL:
 
-        //        break;
-        //    case Item.ITEMRATE.RARE:
-        //        break;
-        //    case Item.ITEMRATE.SR:
-        //        break;
-        //    default:
-        //        break;
-        //}
+        if (second > NonrareSeconds && timeOverNonrare)//指定の時間を過ぎたらレアアイテム出現可能にする
+        {
+            timeOverNonrare = false;
+            popRate[2] = saveRate[2];
+            AverageRate();
+        }
+
+
+
+
+
 
 
 
     }
 
     ////確率を綺麗にする。
-    //private void AverageRate()
-    //{
-    //    for(int i = 0; i < popRate.Length; i++)
-    //    {
-    //        popNum += popRate[i];
-    //    }
-    //    for (int i = 0; i < popRate.Length; i++)
-    //    {
-    //        popRate[i] = (popRate[i] / popNum) * 100;
-    //    }
-        
-    //    Debug.Log(popRate[0]);
-    //    Debug.Log(popRate[1]);
-    //    Debug.Log(popRate[2]);
-    //}
+    private void AverageRate()
+    {
+        for (int i = 0; i < popRate.Length; i++)
+        {
+            popNum += popRate[i];
+        }
+        for (int i = 0; i < popRate.Length; i++)
+        {
+            popRate[i] = (popRate[i] / popNum) * 100;
+        }
+
+        Debug.Log(popRate[0]);
+        Debug.Log(popRate[1]);
+        Debug.Log(popRate[2]);
+    }
 
     ////出現させるレア度を確定させる
     //Item.ITEMRATE RareDecision()
     //{
-        
+
     //    float cnt = Random.Range(0f, popNum);
     //    for (int i = 0; i < popRate.Length; i++)
     //    {
