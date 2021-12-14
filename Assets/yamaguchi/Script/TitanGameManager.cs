7b13ba42148963d451c5d63b12dce53033cf27ad
tai <taikitpmiita@icloud.com>
2021-12-14 11:50:47 +0900

    [update]勝敗つけれるように

---
+++ Assets/yamaguchi/Script/TitanGameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TitanGameManager : MonoBehaviourPunCallbacks
+{
+    private int dieCount;
+
+    public string whichPlayerName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dieCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PCPlayerDie()
+    {
+        dieCount++;
+        if(dieCount>3)
+        {
+            photonView.RPC(nameof(RPCGameResultCheck), RpcTarget.All, "VRPlayer");
+        }
+    }
+
+    public void VRPlayerDie()
+    {
+        dieCount++;
+        photonView.RPC(nameof(RPCGameResultCheck), RpcTarget.All, "PCPlayer");
+    }
+
+    [PunRPC]
+    public void RPCGameResultCheck(string _winPlayer)
+    {
+        if(whichPlayerName == _winPlayer)
+        {
+            Debug.Log("勝ちました");
+        }
+        else
+        {
+            Debug.Log("負けました");
+        }
+    }
+}
